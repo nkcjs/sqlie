fe2da9fd81d138ac90aef146147f8234188fca34
helium <japplet@163.com>
2018-09-10 13:59:30 +0800

    refactor: 取消与 MySQL  的 Connection 相关的方法（connect、end） 1、添加设置数据库连接对象的方法； 2、取消生成数据库连接对象的方法； 3、取消连接数据库方法； 4、取消断开数据库连接的方法。

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,37 @@ export * from './builder/insert';
 export * from './builder/select';
 export * from './builder/update';
 
-import {Connection, ConnectionConfig, createConnection} from 'mysql';
+import {Connection} from 'mysql';
 import {ModelBuilder} from './model';
 
-let connection: Connection = null;
+let $connection: Connection = null;
 
+/**
+ * 创建模型对象
+ * @param {string} table 表名称
+ * @param {string} [pk] 主键名称，默认位 'id'
+ */
 export function createModel(table: string, pk: string = 'id') {
   return new ModelBuilder(table, pk);
 }
 
-export function setOptions(options: ConnectionConfig) {
-  connection = createConnection(options);
+/**
+ * 设置MySQL连接对象
+ * @param connection MySQL连接对象
+ */
+export function setConnection(connection: Connection) {
+  $connection = connection;
 }
 
-export function connectDatabase() {
-  return connection.connect();
-}
-
-export function closeTheConnection() {
-  connection.end();
-}
-
+/**
+ * 执行 SQL 语句
+ * @param {string} sql 被执行语句
+ * @param {Array<string | number>} [params] 执行参数
+ * @returns {Promise<*>}
+ */
 export function execute(sql: string, params: Array<string> = []): Promise<any> {
   return new Promise(function (resolve, reject) {
-    connection.query(sql, params, (err, rows, fields) => {
+    $connection.query(sql, params, (err, rows, fields) => {
       if (err) return reject(err);
       // rows = JSON.parse(JSON.stringify(rows))[0];
       resolve({rows, fields});
